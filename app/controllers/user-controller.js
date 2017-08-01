c0370df1ee92d377f6caba693205f4e1fe630969
jevonthomas <jevon.g.thomas@gmail.com>
2017-08-01 09:09:11 -0500

    initial set up

---
+++ app/controllers/user-controller.js
@@ -0,0 +1,30 @@
+'use strict';
+
+pinterestApp.controller("UserController", function($scope, $window) {
+
+  $scope.account = {
+    email: "",
+    password: ""
+  };
+
+  // $scope.register = () => {
+  //   // TODO validate that user doesn't exist
+  //   console.log("you clicked register");
+  //   UserFactory.createUser($scope.account)
+  //   .then( (userData) => {
+  //     console.log("New User!", userData);
+  //     $scope.login();
+  //   });
+  // };
+
+  // $scope.login = () => {
+  //   UserFactory.loginUser($scope.account)
+  //   .then( (userData) => {
+  //     console.log("userData", userData);
+  //     $window.location.href = '#!/home';
+  //   });
+  // };
+
+
+
+});
